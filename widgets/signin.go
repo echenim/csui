d8643fe63850cc4c755981314ea346f602b57034
echenim <myron.echenim@gmail.com>
2021-11-09 12:30:13 -0800

    sanitizedd the ode

--- widgets/signin.go
+++ widgets/signin.go
@@ -85,60 +85,6 @@ func SignInInput(envMsg, usernameMsg, passwordMsg string, options []string) (str
 	win.SetAlignment(core.AlignCenter)
 	win.Add(rows)
 
-	// env := components.NewListSelect(options)
-	// env.SetSizeStrategy(minSize)
-	// envtext := components.NewText(envMsg)
-	// usernamebox := components.NewInput()
-	// passwordbox := components.NewPasswordInput()
-
-	// envFrame := components.NewFrame(env)
-	// usernameFrame := components.NewFrame(usernamebox)
-	// passwordFrame := components.NewFrame(passwordbox)
-
-	// envtext.PadText(1)
-	// envtext.SetSizeStrategy(minSize)
-	// usernametext := components.NewText(usernameMsg)
-	// usernametext.PadText(1)
-	// usernametext.SetSizeStrategy(minSize)
-	// passwordtext := components.NewText(passwordMsg)
-	// passwordtext.PadText(1)
-	// passwordtext.SetSizeStrategy(minSize)
-
-	// strip := components.NewColumnLayout()
-	// strip.SetSizeStrategy(minSize)
-
-	// var selected bool
-
-	// passwordbox.OnKeypress(func(key *tcell.EventKey) bool {
-	// 	switch key.Key() {
-	// 	case tcell.KeyEnter:
-	// 		selected = true
-	// 		win.Close()
-	// 		return true
-	// 	}
-	// 	return false
-	// })
-
-	// help := components.NewText("ENTER to confirm, ESC to cancel")
-	// help.SetAlignment(core.AlignRight)
-	// help.SetSizeStrategy(core.SizeStrategyMaximumWidth())
-	// help.SetStyle(core.StyleFaint)
-	// strip.Add(help)
-
-	// rows := components.NewRowLayout()
-	// rows.Add(envtext)
-	// rows.Add(envFrame)
-	// rows.Add(usernametext)
-	// rows.Add(usernameFrame)
-	// rows.Add(passwordtext)
-	// rows.Add(passwordFrame)
-	// rows.Add(components.NewSpacer(core.Size{H: 1}))
-	// rows.Add(strip)
-	// rows.SetAlignment(core.AlignCenter)
-
-	// win.SetAlignment(core.AlignCenter)
-	// win.Add(rows)
-
 	if err := win.Show(); err != nil {
 		return "", "", "", err
 	}
